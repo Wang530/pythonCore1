d6a47117965959dc0c778016641d452ae3719c55
root <root>
2020-05-21 10:57:22 +0800

    clone后第一次修改

--- test001/src/cn/wdl/git/FirstClass.java
+++ test001/src/cn/wdl/git/FirstClass.java
@@ -8,7 +8,7 @@ public class FirstClass {
         System.out.println("hello world!");
         System.out.println("hello world!");
         for (int i = 0; i < 5; i++) {
-            System.out.println(i);
+            System.out.println("第"+i+"次修改");
         }
     }
 }
